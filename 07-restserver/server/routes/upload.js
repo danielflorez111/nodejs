248e0b21e52ec5ec3cc2f06a5c2d7d368374e387
danielflorez111 <danielflorez111@hotmail.com>
2018-05-15 21:08:03 -0500

    Restriccion extension

--- 07-restserver/server/routes/upload.js
+++ 07-restserver/server/routes/upload.js
@@ -17,8 +17,23 @@ app.put('/upload', (req, res) => {
     }
 
     let archivo = req.files.archivo;
+    let nombreArchivo = archivo.name.split('.');
+    let extension = nombreArchivo[nombreArchivo.length - 1];
 
-    archivo.mv('./uploads/filename.jpg', (err) => {
+    // Extensiones permitidas
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Extensión no valida',
+                ext: extension
+            }
+        });
+    }
+
+    archivo.mv(`./uploads/${archivo.name}`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
