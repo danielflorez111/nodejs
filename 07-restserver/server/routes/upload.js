675d6d21a95a0aabe1b966d95b2ac64939ab684d
danielflorez111 <danielflorez111@hotmail.com>
2018-05-17 22:09:40 -0500

    File name

--- 07-restserver/server/routes/upload.js
+++ 07-restserver/server/routes/upload.js
@@ -2,10 +2,15 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario');
+
 // default options
 app.use(fileUpload());
 
-app.put('/upload', (req, res) => {
+app.put('/upload/:tipo/:id', (req, res) => {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
 
     if (!req.files) {
         return res.status(400).json({
@@ -16,9 +21,23 @@ app.put('/upload', (req, res) => {
         });
     }
 
+    // Valida tipo
+    let tiposValidos = ['productos', 'usuarios'];
+
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Tipo no valido',
+                ext: tipo
+            }
+        });
+    }
+
+
     let archivo = req.files.archivo;
-    let nombreArchivo = archivo.name.split('.');
-    let extension = nombreArchivo[nombreArchivo.length - 1];
+    let nombre = archivo.name.split('.');
+    let extension = nombre[nombre.length - 1];
 
     // Extensiones permitidas
     let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
@@ -33,7 +52,10 @@ app.put('/upload', (req, res) => {
         });
     }
 
-    archivo.mv(`./uploads/${archivo.name}`, (err) => {
+    // Cambiar nombre al archivo
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
+
+    archivo.mv(`./uploads/${tipo}/${nombreArchivo}`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
