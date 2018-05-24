e70d9ad94e7f873402748c713f83c723d23f9981
danielflorez111 <danielflorez111@hotmail.com>
2018-05-23 22:30:30 -0500

    Chat

--- 10-socket-chat/public/js/socket-chat-jquery.js
+++ 10-socket-chat/public/js/socket-chat-jquery.js
@@ -1,7 +1,15 @@
 var params = new URLSearchParams(window.location.search);
 
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
+
 // Referencias de Jquery
 var divUsuarios = $('#divUsuarios');
+var formEnviar = $('#formEnviar');
+var txtMensaje = $('#txtMensaje');
+var divChatbox = $('#divChatbox');
+
 
 // Funciones para renderizar usuarios
 function renderizarUsuarios(personas) {
@@ -21,6 +29,69 @@ function renderizarUsuarios(personas) {
     divUsuarios.html(html);
 }
 
+function renderizarMensajes(mensaje, yo) {
+
+    var html = '';
+    var fecha = new Date(mensaje.fecha);
+    var hora = fecha.getHours() + ':' + fecha.getMinutes();
+    var adminClass = 'info';
+
+    if (mensaje.nombre === 'Administrador') {
+        adminClass = 'danger';
+    }
+
+    if (yo) {
+        html += '<li class="reverse">';
+        html += '<div class="chat-content">';
+        html += '<h5>' + mensaje.nombre + '</h5>';
+        html += '<div class="box bg-light-inverse">' + mensaje.mensaje + '</div>';
+        html += '</div>';
+        html += '<div class="chat-img">';
+        html += '<img src="assets/images/users/5.jpg" alt="user" />';
+        html += '</div>';
+        html += '<div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+    } else {
+
+        html += '<li class="animated fadeIn">';
+
+        if (mensaje.nombre !== 'Administrador') {
+            html += '<div class="chat-img">';
+            html += '<img src="assets/images/users/1.jpg" alt="user" />';
+            html += '</div>';
+        }
+
+        html += '<div class="chat-content">';
+        html += '<h5>' + mensaje.nombre + '</h5>';
+        html += '<div class="box bg-light-' + adminClass + '">' + mensaje.mensaje + '</div>';
+        html += '</div>';
+        html += '<div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+    }
+
+
+    divChatbox.append(html);
+
+}
+
+
+function scrollBottom() {
+
+    // selectors
+    var newMessage = divChatbox.children('li:last-child');
+
+    // heights
+    var clientHeight = divChatbox.prop('clientHeight');
+    var scrollTop = divChatbox.prop('scrollTop');
+    var scrollHeight = divChatbox.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
+
 
 // Listeners
 
@@ -30,5 +101,23 @@ divUsuarios.on('click', 'a', function () {
     if (id) {
         console.log(id);
     }
-    
+
+});
+
+formEnviar.on('submit', function (e) {
+    e.preventDefault();
+
+    if (txtMensaje.val().trim().length === 0) {
+        return;
+    }
+
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function (mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje, true);
+        scrollBottom();
+    });
+
 });
